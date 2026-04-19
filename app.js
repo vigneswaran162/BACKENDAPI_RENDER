98812e4e8b0e92ce9093f1d45809d7bd2c7092e1
vigneswaran162 <vigneswaran162@gmail.com>
2026-04-19 23:52:09 +0530

    third  commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const connectDB = require("./connectDB"); // ✅ import
+const connectDB = require("./db"); // ✅ import
 
 const app = express();
 
